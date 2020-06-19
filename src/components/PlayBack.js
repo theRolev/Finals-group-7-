5576a940d318c95275359e245099a4cc900f8dd5
h82lin <b04901162@ntu.edu.tw>
2020-06-19 16:21:00 +0800

    Modified PlayBack to include audio player

--- src/components/PlayBack.js
+++ src/components/PlayBack.js
@@ -5,6 +5,8 @@ import CurrentRaw from "./playback/CurrentRaw.js"
 
 import "./css/Playback.css"
 import "../App.css"
+import AudioPlayer, { RHAP_UI } from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 
 function PlayBack() {
     
@@ -19,7 +21,18 @@ function PlayBack() {
 
   return (
     <div className="playback">   
-        {Currt}
+    <div className="audioDiv">
+            <AudioPlayer className="audioPlayer"
+            progressUpdateInterval={50} 
+            src="https://firebasestorage.googleapis.com/v0/b/group7-df874.appspot.com/o/Acejax%20-%20By%20My%20Side%20(ft.%20Danilyon).mp3%2FAcejax%20-%20By%20My%20Side%20(ft.%20Danilyon).mp3?alt=media&token=3e763743-7283-4e7b-86f0-776043158f0a"
+            layout="stacked-reverse"/>
+        </div>
+        <div classNmae="songInfo">
+            <h1 className="title">Song name</h1>
+            <h1 className="artist">Artist</h1>
+            <h1 className="date">Date</h1>
+        </div>
+        
     </div>
   );
 }
